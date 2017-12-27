6e9f60adaf30813708c45653177aeded3be9b867
Scott Johnson <jaywir3@gmail.com>
2017-12-26 18:41:41 -0600

    Export OrganizationRegistry now that it's completed.
    Also:
      - Add a package-lock.json file.

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import Agenda from './Agenda';
 import Member from './Member';
 import Minutes from './Minutes';
 import Organization from './Organization';
+import OrganizationRegistry from './OrganizationRegistry';
 
 export default {
   Agenda,
   Member,
   Minutes,
-  Organization
+  Organization,
+  OrganizationRegistry
 }
